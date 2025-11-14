ce7e95876acbe286252f556f0038b4d0ee885abc
mateusLWD <alves.mateus1050@gmail.com>
2025-11-14 17:01:19 +0000

    .

--- docker/init.js
+++ docker/init.js
@@ -2,7 +2,7 @@ db = db.getSiblingDB("cluster");
 
 db.users.insertMany([
   { name: "Alice", email: "alice@example.com" },
-  { name: "Bob", email: "bob@example.com" }
+  { name: "testing", email: "bob@example.com" }
 ]);
 
 db.products.insertOne({ name: "Widget", price: 9.99 });
