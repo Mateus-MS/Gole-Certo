a266455f3866e5768b413bd8bf056dd77476b92f
mateusLWD <alves.mateus1050@gmail.com>
2025-11-15 16:50:15 +0000

    docker paths updated

--- backend/internal/modules/common/repo/errors.go
+++
@@ -1,7 +0,0 @@
-package generic_repository
-
-import "errors"
-
-var (
-	ErrItemInexistent = errors.New("this generic item does not exists on DB")
-)
--- docker/init.js
+++
@@ -1,8 +0,0 @@
-db = db.getSiblingDB("cluster");
-
-db.users.insertMany([
-  { name: "Alice", email: "alice@example.com" },
-  { name: "testing", email: "bob@example.com" }
-]);
-
-db.products.insertOne({ name: "Widget", price: 9.99 });
