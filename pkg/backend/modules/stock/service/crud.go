70f3222848068d0ff9566192c2f918c59443ef50
mateusLWD <alves.mateus1050@gmail.com>
2025-12-27 22:26:58 +0000

    order subtracting stock

--- pkg/backend/modules/stock/service/crud.go
+++ pkg/backend/modules/stock/service/crud.go
@@ -3,6 +3,7 @@ package stock_service
 import (
 	"context"
 
+	order_error "alves.com/backend/modules/order/errors"
 	stock_model "alves.com/backend/modules/stock/model"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -18,4 +19,16 @@ func (s *service) ReadByID(ctx context.Context, id primitive.ObjectID) (*stock_m
 }
 func (s *service) DeleteByID(ctx context.Context, id primitive.ObjectID) error {
 	return s.repository.DeleteByID(ctx, id)
+}
+func (s *service) UpdateByID(ctx context.Context, stock stock_model.StockEntity) error {
+	return s.repository.UpdateByID(ctx, stock)
+}
+
+func (s *service) AtomicDecreaseStockByID(ctx context.Context, prodID primitive.ObjectID, quantity int) error {
+	result, err := s.repository.AtomicDecreaseStockByID(ctx, prodID, quantity)
+	if result.MatchedCount == 0 {
+		return order_error.ErrUnavaiableQuantity
+	}
+
+	return err
 }
